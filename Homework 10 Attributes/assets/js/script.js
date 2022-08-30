d1888b3c7c9526b1814b33e2fcf69ba1eb0ec14f
hagverdi11 <hagverdiam@code.edu.az>
2022-08-31 02:51:12 +0400

    Added new task

---
+++ Homework 10 Attributes/assets/js/script.js
@@ -0,0 +1,32 @@
+// let text = document.querySelector(".text-input")
+
+// console.log(text.getAttribute("type"));
+
+// text.setAttribute("type","password")
+
+// if(text.getAttribute("type") === "text"){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+// let headers = document.querySelector(".tab-menu .tab-header div")
+// let contents = document.querySelector(".tab-menu .tab-body div")
+
+// headers.forEach(header => {
+//     header.addEventListener("click", function(){
+//         let activeElem = document.querySelector(".active")
+//         activeElem.classList.remove("active")
+//         this.classList.add("active");
+        
+//     })
+// });
+
+let leftTurn = document.querySelector(".icon-left")
+
+let rightTurn = document.querySelector(".icon-right")
+
+rightTurn.addEventListener("click", function(){
+  
+})
